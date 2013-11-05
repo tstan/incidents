2778582d8dc86ac853b4f24d2bdde0e5e8d05ec1
SLUGIS <joe.larson@fire.ca.gov>
2013-11-05 22:28:40 +0000

    Daily update

--- commit.fish
+++ commit.fish
@@ -1,8 +1,8 @@
 #!/usr/bin/fish
 
 git pull
-cp ~/dev/cfslo-incident-viewer/data/* raw/
+cp ~/gst_dashboard/data/* raw/
 python convert.py
 git add -A
-git commit -m "Daily update"
+git commit -m "Daily update" --author "SLUGIS <joe.larson@fire.ca.gov>"
 git push
